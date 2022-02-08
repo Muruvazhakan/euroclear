3f13ea9654e42d5cfdbfa7ec6ebb7de4c563befa
muruvazhakan <puhalmuru@gmail.com>
2022-02-08 19:12:45 +0000

    added icons

---
+++ src/Screen/MainQuery/MainQuery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainQuery() {
+  return <>
+     <h1>MainQuery Screen</h1>
+     <div className="inner">
+     <h1>MainQuery Screen</h1>
+         </div>
+  </>;
+}
+
+export default MainQuery;
